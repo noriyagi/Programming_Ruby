9fcf4d25d5c3a623e8cc79d1aea2de1dc0a0f85b
Noriaki Koyanagi <koyanagn@gmail.com>
2011-06-23 07:34:25 +0900

    P2

---
+++ lib/part1.rb
@@ -0,0 +1,7 @@
+#第１章　イントロダクション
+
+print "1のクラスは: ", 1.class, "\n"
+print "0.0のクラスは: ", 0.0.class, "\n"
+print "trueのクラスは: ", true.class, "\n"
+print "falseのクラスは: ", false.class, "\n"
+print "nilのクラスは: ", nil.class , "\n"
