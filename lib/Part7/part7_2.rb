02579ab480e280fa08ba3a90d4b24a6ada7f8828
Noriaki Koyanagi <koyanagn@gmail.com>
2011-07-31 17:34:32 +0900

    P229

--- lib/Part7/part7_2.rb
+++ lib/Part7/part7_2.rb
@@ -2,7 +2,7 @@
 class Point
 
 	#attr_accessor :x, :y
-	#attr_reader :x, :y
+	attr_reader :x, :y
 
 
 	#7.1.3 Pointの初期化
@@ -15,21 +15,34 @@ class Point
 		"(#@x, #@y)"      #文字列のなかにインスタンス変数を展開している。
 	end
 
-	#アクセッサと属性
-	def x
-		@x
-	end
-	
-	def y
-		@y
+	#アクセッサと属性         -> attr_readerによる置き換え
+#	def x
+#		@x
+#	end
+#	
+#	def y
+#		@y
+#	end
+#
+#	def x=(value)
+#		@x = value
+#	end
+#
+#	def y=(value)
+#		@y = value
+#	end
+
+	#7.1.6 演算子の定義
+	def +(other)          #ベクトルの加算
+		Point.new(@x + other.x, @y + other.y)   #ここで使っている@x,@yはselfのインスタンス変数
+	end						#さらにother側はゲッターメソッドを介している。
+
+	def -@                #座標の符号を反転
+		Point.new(-@x, -@y)
 	end
 
-	def x=(value)
-		@x = value
-	end
-
-	def y=(value)
-		@y = value
+	def *(scalar)
+		Point.new(@x*scalar, @y*scalar)
 	end
 end
 
@@ -37,11 +50,11 @@ p = Point.new(1,2)
 q = Point.new(p.x*2, p.y*3)
 p p.class
 p p.is_a? Point
-puts p
-puts q
+puts p        #(1,2)
+puts q        #(2,6)
 
 q.x = 0       #インスタンス変数にアクセス
 q.y = 0
-puts q
+puts q        #(0,0)
 
 
