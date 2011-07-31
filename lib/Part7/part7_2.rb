d6e3352036c54dcee046f08dbde57af69995fa9a
Noriaki Koyanagi <koyanagn@gmail.com>
2011-07-31 17:19:48 +0900

    P228

--- lib/Part7/part7_2.rb
+++ lib/Part7/part7_2.rb
@@ -1,7 +1,47 @@
 #第7章2週目　クラスとモジュール
 class Point
+
+	#attr_accessor :x, :y
+	#attr_reader :x, :y
+
+
+	#7.1.3 Pointの初期化
+	def initialize(x,y)
+		@x, @y = x, y
+	end
+	
+	#7.1.4 to_sメソッドの定義
+	def to_s
+		"(#@x, #@y)"      #文字列のなかにインスタンス変数を展開している。
+	end
+
+	#アクセッサと属性
+	def x
+		@x
+	end
+	
+	def y
+		@y
+	end
+
+	def x=(value)
+		@x = value
+	end
+
+	def y=(value)
+		@y = value
+	end
 end
 
-p = Point.new
+p = Point.new(1,2)
+q = Point.new(p.x*2, p.y*3)
 p p.class
 p p.is_a? Point
+puts p
+puts q
+
+q.x = 0       #インスタンス変数にアクセス
+q.y = 0
+puts q
+
+
